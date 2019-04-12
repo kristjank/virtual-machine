48d0bb8c7f5f872e8220b88f24f7dfdad1779c0e
Kristjan Kosic - Chris <chris@ark.io>
2019-04-12 22:43:02 +0200

    starting to work on sharing contexts. imports operational

--- packages/core-vm/src/manager.ts
+++ packages/core-vm/src/manager.ts
@@ -1,18 +1,66 @@
 import { app } from "@arkecosystem/core-container";
 import { Logger } from "@arkecosystem/core-interfaces";
-import { Isolate, IsolateOptions } from "isolated-vm";
+// import { Isolate, IsolateOptions, Reference, ExternalCopy } from "isolated-vm";
+// import { Isolate, IsolateOptions, Reference, ExternalCopy } from "isolated-vm";
+import ivm from "isolated-vm";
 
 export class VirtualMachineManager {
     private logger = app.resolvePlugin<Logger.ILogger>("logger");
+    private walletManager = app.resolvePlugin("database").walletManager;
 
     public constructor() {
-        this.logger.info("Starting CORE VIRTUAL MACHINE Engine");
+        this.logger.info("Starting Core Virtual Machine Engine");
+        console.log(this.walletManager.findByAddress("APnhwwyTbMiykJwYbGhYjNgtHiVJDSEhSn"));
     }
 
     public runIvm() {
-        const isolate = new Isolate();
-        console.log("mamqqa");
+        const isolate = new ivm.Isolate({ memoryLimit: 32 });
+
         const context = isolate.createContextSync();
+        const global = context.global;
+        global.setSync("ivm", ivm);
+        isolate
+            .compileScriptSync(
+                `
+function makeReference(ref) {
+return new ivm.Reference(ref);
+}
+function isReference(ref) {
+return ref instanceof ivm.Reference;
+}
+`,
+            )
+            .runSync(context);
+
+        console.log(context.global);
+
         const globalReference = context.globalReference();
+        const wallets = this.walletManager.allByAddress();
+        const data = new ivm.ExternalCopy({ wallets });
+        data.copy({ transferIn: true });
+        console.log(data);
+
+        //    globalReference.setSync('_log', new Reference(function(...args: any) {
+        //             console.log(`Actor : `, ...args);
+        //         }));
+
+        //         globalReference.setSync('global', globalReference.derefInto());
+
+        //         const bootstrap = isolate.compileScriptSync(`
+        // new function() {
+        // const log = _log;
+
+        // global.log = function(...args) {
+        // log.applySync(undefined, args.map(arg => new ivm.ExternalCopy(arg).copyInto()));
+        // }
+        // global.changeSpeed = function(x, y) {
+        // changeSpeed.applySync(undefined, [x, y]);
+        // }
+        // global.getNearbyActors = function() {
+        // return getNearbyActors.applySync(undefined, []);
+        // }
+        // }
+        // `);
+        //         bootstrap.runSync(context);
     }
 }
